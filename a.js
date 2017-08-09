64f1579c347f107227900aad0c975af9ee659631
Jasmeet Singh <jsmitsng143@gmail.com>
2017-08-09 14:21:12 +0530

    Create a.js

---
+++ a.js
@@ -0,0 +1,31 @@
+$(function() {
+  
+  var link = $('#navbar a.dot');
+  
+  link.on('click', function(e) {
+    var target = $($(this).attr('href'));
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 600);
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+  
+  $(window).on('scroll', function(){
+    scrNav();
+  });
+  
+  function scrNav() {
+    var sTop = $(window).scrollTop();
+    $('section').each(function() {
+      var id = $(this).attr('id'),
+          offset = $(this).offset().top-1,
+          height = $(this).height();
+      if(sTop >= offset && sTop < offset + height) {
+        link.removeClass('active');
+        $('#navbar').find('[data-scroll="' + id + '"]').addClass('active');
+      }
+    });
+  }
+  scrNav();
+});
